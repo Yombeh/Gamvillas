ca06d75900085e5b8ff901dd94908475854af457
Hawa Cham <hawacham74@gmail.com>
2025-08-30 16:45:19 +0000

    Add project files.

---
+++ BulkyBookStore/Controllers/VillaController.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+using WhiteLagoon.Domain.Entities;
+using WhiteLagoon.Infrastructure.Data;
+
+namespace BulkyBookStore.Controllers
+{
+
+    public class VillaController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly IWebHostEnvironment  _webHostHostEnvironment;
+        public VillaController(ApplicationDbContext db, IWebHostEnvironment  webHostHostEnvironment)
+        {
+            _db = db;
+            this._webHostHostEnvironment = webHostHostEnvironment;
+        }
+        public IActionResult Index()
+        {
+            var villas = _db.Villas.ToList();
+            return View(villas);
+        }
+
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Villa obj)
+        {
+            if (obj.Name == obj.Description)
+            {
+                ModelState.AddModelError("", "  The description can not be the same as the Name. ");
+            }
+
+
+            if (ModelState.IsValid)
+            {
+                 if(obj.Image != null)
+                {
+                   string fileName = Guid.NewGuid()+Path.GetExtension(obj.Image.Name);
+                    string imagePath = Path.Combine(_webHostHostEnvironment.WebRootPath, @"images\VillaImage");
+
+                    using (var fileStream = new FileStream(Path.Combine(imagePath,fileName), FileMode.Create))
+                    {
+                        obj.Image.CopyTo(fileStream);
+                         
+                        obj.ImageUrl = @"\images\VillaImage\" + fileName;
+                    }
+
+                }
+                 else
+                {
+                    obj.ImageUrl = "https://placehold.co/600x400";
+                }
+
+                _db.Villas.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "The villa has been created successfully";
+
+                return RedirectToAction("Index", "Villa");
+            }
+            return View();
+        }
+
+        public IActionResult Update(int villaId)
+        {
+            Villa? obj = _db.Villas.FirstOrDefault(x => x.Id == villaId);
+            if (obj is null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+            return View(obj);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Villa obj)
+        {
+         
+            if (ModelState.IsValid)
+            {
+
+                _db.Villas.Update(obj);
+                _db.SaveChanges ();
+                TempData["success"] = "The Villa has been updated successfully";
+
+                return RedirectToAction("Index", "Villa");
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int villaId)
+        {
+            Villa? obj = _db.Villas.FirstOrDefault(x => x.Id == villaId);
+            if (obj is null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+            return View(obj);
+        }
+
+
+        [HttpPost]
+        public IActionResult Delete(Villa obj)
+        {
+
+            Villa objFromDb = _db.Villas.FirstOrDefault( u => u.Id == obj.Id);  
+            if (objFromDb is not null )
+            {
+
+                _db.Villas.Remove(objFromDb);
+                _db.SaveChanges();
+                TempData["success"] = "The villa has been deleted successfully.";
+                return RedirectToAction("Index", "Villa");
+            }
+            TempData["error"] = " The villa could not be deleted";
+            return View();
+        }
+
+
+
+    }
+}
---
+++ WhiteLagoon.Domain/Entities/Villa.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhiteLagoon.Domain.Entities
+{
+    public class Villa
+    {
+        public int Id { get; set; }
+        [MaxLength(50)]
+        public required String Name { get; set; }
+        public string?  Description { get; set; }
+
+        [Display(Name = "Price per Night")]
+        [Range(00,100000)]
+        public double  Price  { get; set; }
+        public int Sqft{ get; set; }
+        [Range(1,10)]
+        public int Occupancy { get; set; }
+        [NotMapped]
+        public IFormFile? Image { get; set; }   
+        [Display(Name = "Image Url")]
+        public String? ImageUrl { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+    }
+}
